f6a364bb7a834611744d528f9432937301168e58
kryshe4ka <kryshe4ka@gmail.com>
2021-12-09 09:51:36 +0300

    Initial Commit

---
+++ SteamList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SteamList
+//
+//  Created by Liza Kryshkovskaya on 9.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
